f3f82618c4d64e3a26ec8063a308d9bf58d3d056
Serhii Stupin <sergey.stupin1987@gmail.com>
2020-02-02 11:23:58 +0200

    test added

---
+++ src/test/java/SelenideUI/VetTestExample.java
@@ -0,0 +1,31 @@
+package SelenideUI;
+
+import SelenideUI.Pages.NewVeterPage;
+import SelenideUI.Pages.VeterinariansPage;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VetTestExample {
+    @Test(description = "LastName field validation")
+    @Story("LastName field validation")
+    @Severity(SeverityLevel.TRIVIAL)
+    @TmsLink("vets.com")
+    public void lastNameValidationTests(){
+        String requiredLast = "Last name is required";
+        String lastName = "lastName";
+        String lastNamelongValidation = "Last name must be at least 2 characters long";
+        VeterinariansPage veterinariansPage = new VeterinariansPage();
+        veterinariansPage.openPage()
+                .clickAddBtn()
+                .setLastName("=");
+        NewVeterPage newVeterPage = new NewVeterPage();
+        assertThat(newVeterPage.helpBlockGetText(lastName)).isEqualTo(lastNamelongValidation);
+        newVeterPage.clearLastName();
+        assertThat(newVeterPage.helpBlockGetText(lastName)).isEqualTo(requiredLast);
+    }
+}
