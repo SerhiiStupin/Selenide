59fcd2a6e92fa1a08a76d88671dde1801123bc24
Serhii Stupin <sergey.stupin1987@gmail.com>
2020-02-03 21:18:00 +0200

    Driver fix

--- src/test/java/RemoteWeb/RemoteWeb1.java
+++ src/test/java/RemoteWeb/RemoteWeb1.java
@@ -1,8 +1,14 @@
 package RemoteWeb;
 
+import SelenideUI.Pages.NewVeterPage;
+import SelenideUI.Pages.VeterinariansPage;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.qameta.allure.TmsLink;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -16,6 +22,7 @@ import java.net.URI;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RemoteWeb1 {
     @BeforeClass
@@ -35,23 +42,40 @@ public class RemoteWeb1 {
         options.addArguments("--disable-extensions");
 //        capabilities.setCapability("enableVideo", false);
 //
-        RemoteWebDriver driver = new RemoteWebDriver(
-                URI.create("http://localhost:4444/wd/hub").toURL(),
-                capabilities
-        );
+//        RemoteWebDriver driver = new RemoteWebDriver(
+//                URI.create("http://localhost:4444/wd/hub").toURL(),
+//                capabilities
+//        );
     }
-    @Test
-    public void addNewOwnerTest() {
-//        driver.get("http://localhost:8000/petclinic/owners/add");
-        //open(Configuration.baseUrl);
-        //open("/owners/add");
-        open("http://localhost:8000/petclinic/owners/add");
-        $("h2").shouldHave(Condition.text("New Owner"));
-
-        $(By.id("firstName")).setValue("Selenide");
-        $(By.id("lastName")).setValue("User");
-        $(By.id("address")).setValue("Street");
-        $(By.id("city")).setValue("Dnipro");
-        $(By.id("telephone")).setValue("1234567890");
+    @Test(description = "LastName field validation")
+    @Story("LastName field validation")
+    @Severity(SeverityLevel.TRIVIAL)
+    @TmsLink("vets.com")
+    public void lastNameValidationTests(){
+        String requiredLast = "Last name is required";
+        String lastName = "lastName";
+        String lastNamelongValidation = "Last name must be at least 2 characters long";
+        VeterinariansPage veterinariansPage = new VeterinariansPage();
+        veterinariansPage.openPage()
+                .clickAddBtn()
+                .setLastName("=");
+        NewVeterPage newVeterPage = new NewVeterPage();
+        assertThat(newVeterPage.helpBlockGetText(lastName)).isEqualTo(lastNamelongValidation);
+        newVeterPage.clearLastName();
+        assertThat(newVeterPage.helpBlockGetText(lastName)).isEqualTo(requiredLast);
     }
+//    @Test
+//    public void addNewOwnerTest() {
+////        driver.get("http://localhost:8000/petclinic/owners/add");
+//        //open(Configuration.baseUrl);
+//        //open("/owners/add");
+//        open("http://localhost:8000/petclinic/owners/add");
+//        $("h2").shouldHave(Condition.text("New Owner"));
+//
+//        $(By.id("firstName")).setValue("Selenide");
+//        $(By.id("lastName")).setValue("User");
+//        $(By.id("address")).setValue("Street");
+//        $(By.id("city")).setValue("Dnipro");
+//        $(By.id("telephone")).setValue("1234567890");
+//    }
 }
