5990c8a5928eb6caa56605228706662ea11a31e2
Serhii Stupin <sergey.stupin1987@gmail.com>
2020-02-04 21:05:15 +0200

    localhost change

--- src/test/java/RemoteWeb/RemoteWeb1.java
+++ src/test/java/RemoteWeb/RemoteWeb1.java
@@ -12,75 +12,107 @@ import io.qameta.allure.TmsLink;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.url;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RemoteWeb1 {
-    @BeforeClass
-    public void setUp() throws MalformedURLException {
-        //WebDriverManager.chromedriver().setup();
-        Configuration.remote = "http://localhost:4444/wd/hub";
-        Configuration.browserCapabilities = new DesiredCapabilities();
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("chrome");
-        //capabilities.setVersion("79.0");
-        capabilities.setCapability("enableVNC", true);
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--disable-extensions");
-//        capabilities.setCapability("enableVideo", false);
-//
-        RemoteWebDriver driver = new RemoteWebDriver(
-                URI.create("http://localhost:4444/wd/hub").toURL(),
-                capabilities
-        );
-        Dimension d = new Dimension(1382,744);
-//Resize the current window to the given dimension
-        driver.manage().window().setSize(d);
-    }
-    @Test(description = "Returning to the home page")
-    @Severity(SeverityLevel.MINOR)
-    @Story("Returning to the home page")
-    @TmsLink("vets.com")
-    public void homeButtonTest() {
-        VeterinariansPage veterinariansPage = new VeterinariansPage();
-        veterinariansPage.openPage()
-                .assertUrl(url());
-    }
+        private WebDriver driver;
 
-    @Test(description = "LastName field validation")
-    @Story("LastName field validation")
-    @Severity(SeverityLevel.TRIVIAL)
-    @TmsLink("vets.com")
-    public void lastNameValidationTests(){
-       // WebDriverManager.chromedriver().setup();
-        String requiredLast = "Last name is required";
-        String lastName = "lastName";
-        String lastNamelongValidation = "Last name must be at least 2 characters long";
-        VeterinariansPage veterinariansPage = new VeterinariansPage();
-        veterinariansPage.openPage()
-                .assertUrl(url());
-        veterinariansPage.clickAddBtn()
-                .setLastName("=");
-        NewVeterPage newVeterPage = new NewVeterPage();
-        assertThat(newVeterPage.helpBlockGetText(lastName)).isEqualTo(lastNamelongValidation);
-        newVeterPage.clearLastName();
-        assertThat(newVeterPage.helpBlockGetText(lastName)).isEqualTo(requiredLast);
-    }
+        @BeforeClass
+        public void setUp () throws MalformedURLException {
+//        WebDriverManager.chromedriver().setup();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("chrome");
+//        capabilities.setVersion("78.0");
+            capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", false);
+
+            driver = new RemoteWebDriver(
+                    URI.create("http://localhost:4444/wd/hub").toURL(),
+                    capabilities
+            );
+        }
+
+        @AfterClass
+        public void tearDown () {
+            driver.quit();
+        }
+
+        @Test
+        public void addNewOwnerTest () throws InterruptedException {
+            driver.get("http://139.59.149.247:8000/petclinic/owners/add");
+            sleep(10_000);
+
+            WebElement h2 = driver.findElement(By.cssSelector("h2"));
+            String text = h2.getText();
+            assertThat(text).isEqualTo("New Owner");
+            sleep(10_000);
+        }
+}
+//        //WebDriverManager.chromedriver().setup();
+//        Configuration.remote = "http://localhost:4444/wd/hub";
+//        Configuration.browserCapabilities = new DesiredCapabilities();
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setBrowserName("chrome");
+//        //capabilities.setVersion("79.0");
+//        capabilities.setCapability("enableVNC", true);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--disable-gpu");
+//        options.addArguments("--disable-extensions");
+////        capabilities.setCapability("enableVideo", false);
+////
+//        RemoteWebDriver driver = new RemoteWebDriver(
+//                URI.create("http://localhost:4444/wd/hub").toURL(),
+//                capabilities
+//        );
+//        Dimension d = new Dimension(1382,744);
+////Resize the current window to the given dimension
+//        driver.manage().window().setSize(d);
+//    }
+//    @Test(description = "Returning to the home page")
+//    @Severity(SeverityLevel.MINOR)
+//    @Story("Returning to the home page")
+//    @TmsLink("vets.com")
+//    public void homeButtonTest() {
+//        VeterinariansPage veterinariansPage = new VeterinariansPage();
+//        veterinariansPage.openPage()
+//                .assertUrl(url());
+//    }
+//
+//    @Test(description = "LastName field validation")
+//    @Story("LastName field validation")
+//    @Severity(SeverityLevel.TRIVIAL)
+//    @TmsLink("vets.com")
+//    public void lastNameValidationTests(){
+//       // WebDriverManager.chromedriver().setup();
+//        String requiredLast = "Last name is required";
+//        String lastName = "lastName";
+//        String lastNamelongValidation = "Last name must be at least 2 characters long";
+//        VeterinariansPage veterinariansPage = new VeterinariansPage();
+//        veterinariansPage.openPage()
+//                .assertUrl(url());
+//        veterinariansPage.clickAddBtn()
+//                .setLastName("=");
+//        NewVeterPage newVeterPage = new NewVeterPage();
+//        assertThat(newVeterPage.helpBlockGetText(lastName)).isEqualTo(lastNamelongValidation);
+//        newVeterPage.clearLastName();
+//        assertThat(newVeterPage.helpBlockGetText(lastName)).isEqualTo(requiredLast);
+//    }
 
 
 
@@ -98,4 +130,4 @@ public class RemoteWeb1 {
 //        $(By.id("city")).setValue("Dnipro");
 //        $(By.id("telephone")).setValue("1234567890");
 //    }
-}
+//}
