f7e6d78abeb35765acb0ddbd43ba9170b2e0d405
Serhii Stupin <sergey.stupin1987@gmail.com>
2020-02-04 20:34:24 +0200

    localhost change

--- src/test/java/RemoteWeb/RemoteWeb1.java
+++ src/test/java/RemoteWeb/RemoteWeb1.java
@@ -33,8 +33,8 @@ public class RemoteWeb1 {
         Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browserCapabilities = new DesiredCapabilities();
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName("firefox");
-        capabilities.setVersion("71.0");
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("79.0");
        // capabilities.setCapability("enableVNC", true);
        // ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--headless");
